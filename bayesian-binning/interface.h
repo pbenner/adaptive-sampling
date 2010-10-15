39a98884b29872759634b3c09ff644a5e32e424c
Philipp Benner <benner@mis.mpg.de>
2010-10-15 10:26:01 +0200

    2010/10/15-1

--- bayesian-binning/interface.h
+++ bayesian-binning/interface.h
@@ -10,4 +10,9 @@ extern void freeMatrix(Matrix *m);
 extern Vector * allocVector(int size);
 extern Matrix * allocMatrix(int rows, int columns);
 
+extern Vector * fromGslVector(const gsl_vector * vector);
+extern Matrix * fromGslMatrix(const gsl_matrix * matrix);
+extern gsl_vector * toGslVector(const Vector *vector);
+extern gsl_matrix * toGslMatrix(const Matrix *matrix);
+
 extern Matrix * binning(Vector *successes, Vector *failures, Vector *prior, Options *options);
