1e90fc6f4eebaae924dd5eae7b3da59f6e9ff529
Philipp Benner <philipp.benner@mis.mpg.de>
2011-04-14 11:37:25 +0200

    2011/04/14-11:32:29 (Linux x4140-2 unknown)

--- bayesian_binning/interface.h
+++ bayesian_binning/interface.h
@@ -1,18 +1,31 @@
+/* Copyright (C) 2010 Philipp Benner
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef INTERFACE_H
+#define INTERFACE_H
 
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_odeiv.h>
 
 #include <bayes_datatypes.h>
-
-extern void freeVector(Vector *v);
-extern void freeMatrix(Matrix *m);
-extern Vector * allocVector(int size);
-extern Matrix * allocMatrix(int rows, int columns);
-
-extern Vector * fromGslVector(const gsl_vector * vector);
-extern Matrix * fromGslMatrix(const gsl_matrix * matrix);
-extern gsl_vector * toGslVector(const Vector *vector);
-extern gsl_matrix * toGslMatrix(const Matrix *matrix);
+#include <bayes_linalg.h>
 
 extern BinningResult * binning(Matrix *counts, Matrix *alpha, Vector *prior, Options *options);
+
+#endif /* INTERFACE_H */
+
