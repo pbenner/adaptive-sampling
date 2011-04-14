25c517248c3f3fb830ede32903303a8b692f858a
Philipp Benner <philipp.benner@mis.mpg.de>
2011-04-14 15:32:30 +0200

    2011/04/14-15:19:56 (Linux x4140-1 unknown)

---
+++ dpm/gaussian-dpm.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+
+# imports
+################################################################################
+
+import math
+
+import numpy               as     np
+import numpy.random.mtrand as     mt
+import numpy.random        as     rd
+
+from   matplotlib          import *
+from   matplotlib.pyplot   import *
+from   matplotlib.image    import NonUniformImage
+import matplotlib.patches  as     patches
+import matplotlib.path     as     path
+
+# local imports
+################################################################################
+
+from interface  import *
+
+# Gaussian DPM
+################################################################################
+
+class GaussianDPM():
+    def __init__(self, n, k):
+        dpm_init(n, k)
+        dpm_print()
+    def num_clusters(self):
+        dpm_num_clusters()
+    def sample(self, n):
+        dpm_sample(n)
+    def plot(self, ax):
+        num_clusters = dpm_num_clusters()
+        for c in range(0, num_clusters):
+            ax.scatter(dpm_cluster(c), c=tuple(rd.rand(3)))
+
+def main():
+    dpm = GaussianDPM(10, 4)
+
+    fig = figure()
+    ax1 = fig.add_subplot(2,1,1, title="Data")
+    ax2 = fig.add_subplot(2,1,2, title="Clustering Result K="+str(dpm.num_clusters()))
+    dpm.plot(ax2)
+    show()
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ dpm/interface.cc
@@ -0,0 +1,92 @@
+/* Copyright (C) 2011 Philipp Benner
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include <gsl/gsl_matrix.h>
+
+namespace Bayes {
+        extern "C" {
+#include <bayes_linalg.h>
+        }
+}
+
+#include "init.hh"
+#include "cluster.hh"
+#include "gaussian-data.hh"
+#include "gaussian-dpm.hh"
+#include "interface.hh"
+
+static GaussianDPM* _gdpm;
+
+extern "C" {
+
+Bayes::Vector * _allocVector(int size)              { return Bayes::allocVector(size); }
+void            _freeVector(Bayes::Vector *v)       { Bayes::freeVector(v); }
+Bayes::Matrix * _allocMatrix(int rows, int columns) { return Bayes::allocMatrix(rows, columns); }
+void            _freeMatrix(Bayes::Matrix *m)       { Bayes::freeMatrix(m); }
+void            _free(void *ptr)                    { free(ptr); }
+
+void _dpm_init(unsigned int n, unsigned int k) {
+        __dpm_init__();
+
+        gsl_matrix* cov = gsl_matrix_alloc(2,2);
+        gsl_matrix_set(cov, 0, 0, 0.5);
+        gsl_matrix_set(cov, 1, 1, 0.5);
+        gsl_matrix_set(cov, 0, 1, 0.2);
+        gsl_matrix_set(cov, 1, 0, 0.2);
+
+        GaussianData data(cov, n, k);
+        _gdpm = new GaussianDPM(data);
+}
+
+unsigned int _dpm_num_clusters() {
+        return _gdpm->num_clusters();
+}
+
+Bayes::Matrix* _dpm_cluster(unsigned int c) {
+        Cluster::cluster& cl = (*_gdpm)[c];
+        int n = cl.elements.size();
+        int m = 2;
+
+        Bayes::Matrix* result = Bayes::allocMatrix(n, m);
+        int i = 0;
+        for (Cluster::elements_t::iterator it = cl.elements.begin();
+             it != cl.elements.end(); it++) {
+                result->mat[i][0] = (*it)->x[0];
+                result->mat[i][1] = (*it)->x[1];
+                i++;
+        }
+
+        return result;
+}
+
+void _dpm_print() {
+        cout << *_gdpm << endl;
+}
+
+void _dpm_sample(unsigned int n) {
+        _gdpm->gibbsSample(n);
+}
+
+void _dpm_free() {
+        delete(_gdpm);
+}
+
+}
