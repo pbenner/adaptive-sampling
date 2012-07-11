bb50fccf4fd217c83e5ae244d80294b109a9415d
Philipp Benner <philipp.benner@mis.mpg.de>
2012-07-11 14:30:28 +0200

    2012/07/11-14:30:28 (Darwin sirius.local i386)

--- math/hmm-sampler.R
+++ math/hmm-sampler.R
@@ -1,46 +1,35 @@
 
-# forward recursion
+# forward recursion (marginal posterior)
 ################################################################################
 
-phi <- function(parameter, result, i, j) {
-  a1 <- parameter$alpha.success[i]
-  a2 <- parameter$alpha.failure[i]
-  n1 <- parameter$alpha.success[i] + sum(parameter$success[i:j])
-  n2 <- parameter$alpha.failure[i] + sum(parameter$failure[i:j])
+phi <- function(parameter, phi.result, j, h) {
+  a1 <- parameter$alpha.success[1]
+  a2 <- parameter$alpha.failure[1]
+  n1 <- parameter$alpha.success[1] + sum(parameter$success[1:j])
+  n2 <- parameter$alpha.failure[1] + sum(parameter$failure[1:j])
 
-  if (i == 1) {
-    return(beta(n1, n2)/beta(a1, a2))
-  }
-  else {
-#    return(parameter$rho * result[i-1,j] + (1-parameter$rho) * beta(n1, n2)/beta(a1, a2) * result[i-1,i-1])
-    return(parameter$rho/result[i-1,i-1] * result[i-1,j] + (1-parameter$rho) * beta(n1, n2)/beta(a1, a2))
-  }
-}
-phi.init <- function(parameter) {
-  result <- matrix(0, parameter$L, parameter$L)
-  for (i in 1:parameter$L) {
-    for (j in i:parameter$L) {
-      result[i,j] <- phi(parameter, result, i, j)
+  result <- parameter$rho^(j-1)*beta(n1, n2)/beta(a1, a2)*h(1, j, n1, n2)
+  if (j > 1) {
+    for (k in 1:(j-1)) {
+      a1 <- parameter$alpha.success[k+1]
+      a2 <- parameter$alpha.failure[k+1]
+      n1 <- parameter$alpha.success[k+1] + sum(parameter$success[(k+1):j])
+      n2 <- parameter$alpha.failure[k+1] + sum(parameter$failure[(k+1):j])
+      result <- result + (1-parameter$rho)*parameter$rho^(j-k-1)*phi.result[k]*beta(n1, n2)/beta(a1, a2)*h(k+1, j, n1, n2)
     }
   }
   return(result)
 }
-
-f.forward.prime <- function(parameter, i,j, h) {
-  a1 <- parameter$alpha.success[i]
-  a2 <- parameter$alpha.failure[i]
-  n1 <- parameter$alpha.success[i] + sum(parameter$success[i:j])
-  n2 <- parameter$alpha.failure[i] + sum(parameter$failure[i:j])
-  if(i == 1) {
-    return(beta(n1, n2)/beta(a1, a2)*h(i, j, n1,n2))
+phi.init <- function(parameter) {
+  result <- rep(0, parameter$L)
+  h <- function(i, j, n1, n2) 1
+  for (j in 1:parameter$L) {
+    result[j] <- phi(parameter, result, j, h)
   }
-  else {
-#    return(parameter$rho * f.forward.prime(parameter, i-1,j, h) + (1-parameter$rho) * beta(n1, n2)/beta(a1, a2)*parameter$phi[i-1,i-1]*h(i, j, n1,n2))
-    return(parameter$rho/parameter$phi[i-1,i-1] * f.forward.prime(parameter, i-1,j, h) + (1-parameter$rho) * beta(n1, n2)/beta(a1, a2)*h(i, j, n1,n2))
-  }
+  return(result)
 }
 f.forward <- function(parameter, j, h) {
-  return (f.forward.prime(parameter, j,j, h)/parameter$phi[j,j])
+  return (phi(parameter, parameter$phi, j, h)/parameter$phi[j])
 }
 f.forward.expectation <- function(parameter, j) {
   h <- function(i, j, n1, n2) c(n1/(n1+n2), n2/(n1+n2))
@@ -51,53 +40,59 @@ f.forward.stddev <- function(parameter, j) {
   return(sqrt(f.forward(parameter, j, h)))
 }
 
-# backward recursion
+# backward recursion (marginal posterior)
 ################################################################################
 
-psi <- function(parameter, result, j, k) {
+psi <- function(parameter, psi.result, j, h) {
   a1 <- parameter$alpha.success[j]
   a2 <- parameter$alpha.failure[j]
-  n1 <- parameter$alpha.success[j] + sum(parameter$success[j:k])
-  n2 <- parameter$alpha.failure[j] + sum(parameter$failure[j:k])
+  n1 <- parameter$alpha.success[j] + sum(parameter$success[j:parameter$L])
+  n2 <- parameter$alpha.failure[j] + sum(parameter$failure[j:parameter$L])
 
-  if (k == parameter$L) {
-    return (beta(n1, n2)/beta(a1, a2))
-  }
-  else {
-    return (parameter$rho * result[j,k+1] + (1-parameter$rho) * beta(n1, n2)/beta(a1, a2) * result[j+1,k+1])
-  }
-}
-psi.init <- function(parameter, result) {
-  result <- matrix(0, parameter$L, parameter$L)
-  for (j in parameter$L:1) {
-    for (k in parameter$L:j) {
-      result[j,k] <- psi(parameter, result, j, k)
+  result <- parameter$rho^(parameter$L-j)*beta(n1, n2)/beta(a1, a2)*h(j, parameter$L, n1, n2)
+  if (j+1 <= parameter$L) {
+    for (k in (j+1):parameter$L) {
+      a1 <- parameter$alpha.success[j]
+      a2 <- parameter$alpha.failure[j]
+      n1 <- parameter$alpha.success[j] + sum(parameter$success[j:(k-1)])
+      n2 <- parameter$alpha.failure[j] + sum(parameter$failure[j:(k-1)])
+      result <- result + (1-parameter$rho)*parameter$rho^(k-1-j)*psi.result[k]*beta(n1, n2)/beta(a1, a2)*h(j, k-1, n1, n2)
     }
   }
   return(result)
 }
+psi.init <- function(parameter) {
+  result <- rep(0, parameter$L)
+  h <- function(i, j, n1, n2) 1
+  for (j in parameter$L:1) {
+    result[j] <- psi(parameter, result, j, h)
+  }
+  return(result)
+}
 
-# forward and backward recursion
+# forward and backward recursion (marginal posterior)
 ################################################################################
 
-g <- function(parameter, j, k, h) {
-  if (k == parameter$L) {
-    return (f.forward.prime(parameter, j,k, h))
+g <- function(parameter, j, h) {
+  result <- parameter$rho^(parameter$L-j)*phi(parameter, parameter$phi, j, h)
+  if (j+1 <= parameter$L) {
+    for (k in (j+1):parameter$L) {
+      result <- result + (1-parameter$rho)*parameter$rho^(k-1-j)*phi(parameter, parameter$phi, k-1, h)*parameter$psi[k]
+    }
   }
-  else {
-    return (parameter$rho * g(parameter, j, k+1, h) + (1-parameter$rho)*f.forward.prime(parameter, j,k, h)*parameter$psi[j+1,k+1])
-  }
+  return(result)
 }
-g.norm <- function(parameter, j, k) {
-  if (k == parameter$L) {
-    return (parameter$phi[j,k])
+g.norm <- function(parameter, j) {
+  result <- parameter$rho^(parameter$L-j)*parameter$phi[j]
+  if (j+1 <= parameter$L) {
+    for (k in (j+1):parameter$L) {
+      result <- result + (1-parameter$rho)*parameter$rho^(k-1-j)*parameter$phi[k-1]*parameter$psi[k]
+    }
   }
-  else {
-    return (parameter$rho * g.norm(parameter, j, k+1) + (1-parameter$rho)*parameter$phi[j,k]*parameter$psi[j+1,k+1])
-  }
+  return(result)
 }
-f <- function(parameter, j,h) {
-  return (g(parameter, j,j,h)/g.norm(parameter, j,j))
+f <- function(parameter, j, h) {
+  return (g(parameter, j, h)/g.norm(parameter, j))
 }
 f.expectation <- function(parameter, j) {
   h <- function(i, j, n1, n2) c(n1/(n1+n2), n2/(n1+n2))
@@ -129,32 +124,16 @@ parameter.init <- function(parameter, extended=TRUE) {
   return(parameter)
 }
 
-# utility
+# utility (full posterior)
 ################################################################################
 
-f.utility.rec.prime <- function(parameter, result, i, j, h) {
-  a1 <- parameter$alpha.success[i]
-  a2 <- parameter$alpha.failure[i]
-  n1 <- parameter$alpha.success[i] + sum(parameter$success[i:j])
-  n2 <- parameter$alpha.failure[i] + sum(parameter$failure[i:j])
-  
-  if (i == 1) {
-    return(beta(n1, n2)/beta(a1, a2)*h(i, j, n1,n2))
+f.utility.rec <- function(parameter, j, h) {
+  result <- rep(0, parameter$L)
+  for (j in 1:parameter$L) {
+    result[j] <- phi(parameter, result, j, h)
   }
-  else {
-    return(parameter$rho * result[i-1,j] + (1-parameter$rho) * beta(n1, n2)/beta(a1, a2)*result[i-1,i-1]*h(i, j, n1,n2))
-  }
+  return(result[parameter$L])
 }
-f.utility.rec <- function(parameter, i, j, h) {
-  result <- matrix(0, parameter$L, parameter$L)
-  for (i in 1:parameter$L) {
-    for (j in i:parameter$L) {
-      result[i,j] <- f.utility.rec.prime(parameter, result, i, j, h)
-    }
-  }
-  return(result[parameter$L,parameter$L])
-}
-
 f.utility.prime <- function(parameter, j, l) {
   L <- parameter$L
   parameter.new <- parameter
@@ -182,14 +161,11 @@ f.utility.prime <- function(parameter, j, l) {
   }
   parameter.new <- parameter.init(parameter.new, FALSE)
 
-  result <- log(parameter$phi[1,1])-log(parameter.new$phi[1, 1])
-  ml2    <- parameter.new$phi[1, 1]
-  for (i in 2:L) {
-    result <- result + log(parameter$phi[i,i])-log(parameter.new$phi[i, i])
-    ml2    <- ml2*parameter.new$phi[i, i]
-  }
-  result <- result + f.utility.rec(parameter.new, L, L, h)/ml2
-  
+  result <- log(parameter$phi[L])-log(parameter.new$phi[L])
+  print(result)
+  print(f.utility.rec(parameter.new, L, h))
+  print(parameter.new$phi[L])
+  result <- result + f.utility.rec(parameter.new, L, h)/parameter.new$phi[L]
   return(result)
 }
 
@@ -268,8 +244,8 @@ sample.prime <- function(parameter) {
 }
 
 sample <- function(parameter, n, video=FALSE) {
+  parameter <- parameter.init(parameter)
   if (n > 0) {
-    parameter <- parameter.init(parameter)
     for (i in 1:n) {
       parameter <- sample.prime(parameter)
       print(parameter$utility)
@@ -297,6 +273,6 @@ parameter   <- list(rho = 0.4,
                     failure = rep(0, length(groundtruth))
                     )
 
-parameter <- sample(parameter, 250, video=FALSE)
+parameter <- sample(parameter, 250, video=TRUE)
 
 #mencoder mf://plot_*.png -mf type=png:fps=4 -ovc lavc -lavcopts vcodec=mpeg4 -oac copy -o plot.avi
