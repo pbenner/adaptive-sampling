5ea55bd49bac1a481735c27406c8c1d1df5f0c00
Philipp Benner <philipp.benner@mis.mpg.de>
2012-07-22 12:34:57 +0200

    2012/07/22-12:34:57 (Linux thalamus unknown)

--- adaptive_sampling/interface.py
+++ adaptive_sampling/interface.py
@@ -146,6 +146,9 @@ _lib._free_.argtype         = []
 _lib.binning.restype        = POINTER(BINNING_RESULT)
 _lib.binning.argtypes       = [c_int, POINTER(POINTER(MATRIX)), POINTER(POINTER(MATRIX)), POINTER(VECTOR), POINTER(MATRIX), POINTER(OPTIONS)]
 
+_lib.utility.restype        = POINTER(VECTOR)
+_lib.utility.argtypes       = [c_int, POINTER(POINTER(MATRIX)), POINTER(POINTER(MATRIX)), POINTER(POINTER(MATRIX)), POINTER(VECTOR), POINTER(MATRIX), POINTER(OPTIONS)]
+
 # convert datatypes
 # ------------------------------------------------------------------------------
 
@@ -225,3 +228,34 @@ def binning(events, counts, alpha, beta, gamma, options):
      _lib._free(tmp)
 
      return result
+
+def utility(events, counts, counts_diff, alpha, beta, gamma, options):
+     c_events      = c_int(events)
+     c_counts      = (events*POINTER(MATRIX))()
+     c_counts_diff = (events*POINTER(MATRIX))()
+     c_alpha       = (events*POINTER(MATRIX))()
+     for i in range(0, events):
+          c_counts[i]  = _lib._alloc_matrix(len(counts[i]), len(counts[i][0]))
+          copyMatrixToC(counts[i], c_counts[i])
+          c_counts_diff[i]  = _lib._alloc_matrix(len(counts_diff[i]), len(counts_diff[i][0]))
+          copyMatrixToC(counts_diff[i], c_counts_diff[i])
+          c_alpha[i]   = _lib._alloc_matrix(len(alpha[i]), len(alpha[i][0]))
+          copyMatrixToC(alpha[i],  c_alpha[i])
+     c_beta  = _lib._alloc_vector(len(beta))
+     copyVectorToC(beta,  c_beta)
+     c_gamma = _lib._alloc_matrix(len(gamma), len(gamma[0]))
+     copyMatrixToC(gamma,  c_gamma)
+     c_options = pointer(OPTIONS(options))
+
+     result  = _lib.utility(c_events, c_counts, c_counts_diff, c_alpha, c_beta, c_gamma, c_options)
+     utility = getVector(result)
+
+     for i in range(0, events):
+          _lib._free_matrix(c_counts[i])
+          _lib._free_matrix(c_counts_diff[i])
+          _lib._free_matrix(c_alpha[i])
+     _lib._free_vector(c_beta)
+     _lib._free_matrix(c_gamma)
+     _lib._free_vector(result)
+
+     return utility
