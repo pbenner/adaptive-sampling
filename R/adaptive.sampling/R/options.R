7d3818596914d901b2ea002ba9cd4785b3685ecb
Philipp Benner <philipp.benner@mis.mpg.de>
2012-09-26 23:39:07 +0200

    2012/09/26-23:39:06 (Linux thalamus unknown)

--- R/adaptive.sampling/R/options.R
+++ R/adaptive.sampling/R/options.R
@@ -1,4 +1,4 @@
-# Copyright (C) 2011, 2012 Tobias Elze
+# Copyright (C) 2011, 2012 Tobias Elze, Philipp Benner
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,6 +13,31 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#' Returns a default set of parameters for the binning and sampling algorithm.
+#'
+#' @param n.moments number of raw moments
+#' @param model.posterior whether or not to compute the model posterior
+#' @param bprob whether or not to compute the break probabilities
+#' @param kl.psi compute the psi Kullback-Leibler divergence
+#' @param kl.multibin compute the multibin Kullback-Leibler divergence
+#' @param effective.counts compute the effective counts
+#' @param effective.posterior.counts compute the effective posterior counts
+#' @param density whether or not to compute the density
+#' @param density.step step size for computing the density
+#' @param density.range limits the range within which the density is computed
+#' @param threads number of threads that are used for computation
+#' @param stacksize stacksize limit for multiple pthreads
+#' @param algorithm 0: prombs, 1: multibin sampler
+#' @param which specify the response for which all quantities are computed
+#' @param hmm if 1 then hidden Markov models are used instead
+#' @param rho cohesion parameter for the hidden Markov model 
+#' @param samples the number of multibin samples for algorithm=1,
+#' the first component of the vector specifies the number of burn-in samples
+#' @examples
+#' options <- make.options(model.posterior=0)
+#' ls.str(options)
+#' @export
 
 make.options <-
   function(n.moments=2,
