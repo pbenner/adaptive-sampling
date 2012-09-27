8491a7a4aaa9e8c00185056a6d1bab0ef2468d36
Philipp Benner <philipp.benner@mis.mpg.de>
2012-09-27 13:42:37 +0200

    2012/09/27-13:42:36 (Linux x4140-7 unknown)

--- R/multibin.summation/demo/prombsTest.R
+++ R/multibin.summation/demo/prombsTest.R
@@ -14,16 +14,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-require(prombs)
+require(multibin.summation)
 
-prombsExtended.demo <- function(m = 0) {
+prombs.demo <- function(m = 0) {
   g = c(1, 2, 3, 4, 5)
   f = c(1, 2, 3, 4, 5,
-        0, 1, 2, 3, 4,
-        0, 0, 1, 2, 3,
-        0, 0, 0, 1, 2,
-        0, 0, 0, 0, 1)
-  h = c(1, 2, 3, 4, 5,
         0, 1, 2, 3, 4,
         0, 0, 1, 2, 3,
         0, 0, 0, 1, 2,
@@ -32,12 +27,9 @@ prombsExtended.demo <- function(m = 0) {
   dim(f) <- c(5,5)
   f      <- t(f)
 
-  dim(h) <- c(5,5)
-  h      <- t(h)
-
-  # correct answer is [25.0063 200.0500 315.0788 160.0400 25.0063]
+  # correct answer is [5 40 63 32 5]
   #
-  exp(prombsExtended(log(g), log(f), h, 0.0001, m));
+  exp(prombs(log(g), log(f), m));
 }
 
-result <- prombsExtended.demo()
+result <- prombs.demo()
